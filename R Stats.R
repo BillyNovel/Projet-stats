ef9db1adb500314855a85da295d7aba82c655d3f
BillyNovel <fcafel@hotmail.fr>
2018-01-20 16:54:03 +0100

    Cluster ANOVA

--- R Stats.R
+++ R Stats.R
@@ -101,8 +101,10 @@ plot(deptclass,   col=deptclass$`TPCluster$cluster`,border = col,  lwd=.1, add=T
 
 dev.off()
 
-Data_Vins=read.table(file='C:/Users/Olhagaray/Documents/GitHub/Projet-stats/Data_Vins.csv', skip=0, header=TRUE, sep = ';', row.names =1 )
+Data_Vins=read.table("Data_Vins.csv", skip=0, header=TRUE, sep = ';', row.names =1 )
 an<-lm(formula=total~Cluster,data=Data_Vins)
+an1<-lm(formula=total~1,data= Data_Vins)
+anova(an,an1)
 summary(an)
 
 total.lm=lm(total~nombre_declarations+superficie+superficie_aop+superficie_cognac+superficie_igp+superficie_vsig,data=vins)
